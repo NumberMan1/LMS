d6cfea312480a7f68d490695c44170817bc4dce9
NumberMan <2100600995@qq.com>
2023-06-08 02:48:01 +0800

    后台完整实现

--- LMS/fcgi/lms_borrow_action/project/src/lms_func.cc
+++ LMS/fcgi/lms_borrow_action/project/src/lms_func.cc
@@ -2,8 +2,8 @@
 
 namespace lms {
 
-void SearchBindArgs(const Json::Value &value,
-        mysqlx::SqlStatement *state, bool type_is_all, int arg_num) {
+void SearchBindArgs(const Json::Value &value, mysqlx::SqlStatement *state,
+        bool type_is_all, int arg_num) noexcept {
     std::string text{"%"};
     text += value["text"].asString();
     text += '%';
@@ -40,8 +40,8 @@ void SearchBindArgs(const Json::Value &value,
     }
 }
 
-void SearchBindArgs(const Json::Value &value,
-        mysqlx::SqlStatement *state, int arg_num) {
+void SearchBindArgs(const Json::Value &value, mysqlx::SqlStatement *state,
+        int arg_num) noexcept {
     std::string text{"%"};
     text += value["text"].asString();
     text += '%';
@@ -61,12 +61,24 @@ void SearchBindArgs(const Json::Value &value,
     }
 }
 
-bool DoubleIsZero(double num) {
+bool DoubleIsZero(double num) noexcept {
     if (std::abs(num) < 1e-8) {
         return true;
     } else {
         return false;
     }
+}
+
+std::string GetNow() noexcept {
+    using namespace std::chrono;
+    auto &&time = system_clock::now();
+    time_t &&tt = system_clock::to_time_t(time);
+    auto &&time_tm = localtime(&tt);
+    std::string str;
+    str.resize(10);
+    std::sprintf(str.data(), "%d-%02d-%02d",
+        time_tm->tm_year + 1900, time_tm->tm_mon + 1, time_tm->tm_mday);
+    return str;
 }
     
 } // namespace lms
