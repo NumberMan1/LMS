bb356ea3fb5fb15e8997f9c3e3e5022d2b13788e
NumberMan <2100600995@qq.com>
2023-06-18 18:39:00 +0800

    增加了注册的验证

--- LMS/lms_main_window.cc
+++ LMS/lms_main_window.cc
@@ -598,6 +598,18 @@ void LMSMainWindow::on_user_sign_up_yse_btn_clicked() {
         box->exec();
         return;
     }
+    if (ui_.user_sign_up_name_edit->text().isEmpty()) {
+        box->setText("请输入姓名");
+        box->exec();
+        return;
+    }    
+    re = R"cpp(^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$)cpp";
+    if (!std::regex_match(ui_.user_sign_up_tel_edit->text().toStdString(),
+        re)) {
+        box->setText("请输入正确的电话号码");
+        box->exec();
+        return;
+    }
     re = R"cpp(^\S{6,}$)cpp";
     if (!std::regex_match(ui_.user_sign_up_pwd_line_edit->text().toStdString(),
          re)) {
